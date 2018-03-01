a2d8f10925a551f832b46e1bfdb9156d1c097f0d
Prakash003 <35654242+Prakash003@users.noreply.github.com>
2018-03-01 16:16:57 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,r,t,s=0;
+printf("Enter n value");
+scanf("%d",&n);
+t=n;
+while(t!=0)
+{
+r=t%n;
+t=t/10;
+s=s*10+r;
+}
+if(s==n)
+{
+printf("Palinrome");
+}
+else
+{
+printf("Not a Palinrome");
+}
+getch();
+}
