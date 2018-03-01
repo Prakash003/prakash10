1b08b17390ccb60663eae988e5ba69e774446441
Prakash003 <35654242+Prakash003@users.noreply.github.com>
2018-03-01 16:50:15 +0530

    Create printodd.c

---
+++ printodd.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+void main()
+{
+int a,b,i;
+printf("Enter the intervals a and b:");
+scanf("%d%d",&a,&b);
+  printf("The odd numbers are");
+for(i=a;i<=b;i++)
+{
+if(i%2!=0)
+printf("\t%d",i);
+}
+}
