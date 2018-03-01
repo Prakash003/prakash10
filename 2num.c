636c369921d3504425b4cd2e124acfbd11ce22e9
Prakash003 <35654242+Prakash003@users.noreply.github.com>
2018-03-01 15:39:47 +0530

    Create 2num.c

---
+++ 2num.c
@@ -0,0 +1,20 @@
+#include<stdio.h>   
+void main(){
+
+int a,b,c;
+
+clrscr();
+
+printf("enter first number");
+
+scanf("%d",&a);
+
+printf("enter second number");
+
+scanf("%d",&b);
+
+c=a+b;
+
+printf("total number is %d",c);
+
+}
