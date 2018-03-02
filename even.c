46d3cac23484184a21259c3516a3111b0657810f
Prakash003 <35654242+Prakash003@users.noreply.github.com>
2018-03-02 14:21:32 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int x,y,rem,i;
+printf(" Enter the values of X and Y");
+scanf("%d %d",&x,&y);
+printf("\n even numbers between %d %d"are, x,y );
+for(i=x;i<=y;++i)
+rem=i%2;
+if rem==0
+{
+printf("\n %d",i);
+}
+getch();
+}
